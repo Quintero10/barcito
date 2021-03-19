fd04f0b44e5dd980b51a705914438a464526592f
AgustínMoraguesBeckar <amoragues@axxonconsulting.com>
2021-03-19 19:12:48 -0300

    trabajo de funciones para poblar el dropdown

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { SearchBox,ISearchBoxStyles  } from 'office-ui-fabric-react/lib/SearchBox';
 import { PrimaryButton, IContextualMenuProps, Stack, IStackTokens } from 'office-ui-fabric-react';
 import { ComboBox, DefaultPalette, Dropdown, DropdownMenuItemType, IComboBoxOption, IDropdownOption, IDropdownStyles, IStackItemStyles, SelectableOptionMenuItemType, Toggle } from '@fluentui/react';
-import { getGlasses } from './Utils/Utils';
+import { callbackGetGlasses, getGlasses } from './Utils/Utils';
 
 const Search = (props:any) => {
 
@@ -32,19 +32,22 @@ const Search = (props:any) => {
     };
     
     const options: IDropdownOption[] = [
-      { key: 'fruitsHeader', text: 'Fruits', itemType: DropdownMenuItemType.Header },
+      { key: 'glasses', text: 'Glasses', itemType: DropdownMenuItemType.Header },
       { key: 'apple', text: 'Apple' },
-      { key: 'banana', text: 'Banana' },
-      { key: 'orange', text: 'Orange', disabled: true },
-      { key: 'grape', text: 'Grape' },
-      { key: 'divider_1', text: '-', itemType: DropdownMenuItemType.Divider },
-      { key: 'vegetablesHeader', text: 'Vegetables', itemType: DropdownMenuItemType.Header },
-      { key: 'broccoli', text: 'Broccoli' },
-      { key: 'carrot', text: 'Carrot' },
-      { key: 'lettuce', text: 'Lettuce' },
+     
     ];
 
- 
+    function poblateDropdown(){
+      debugger;
+       let glasses: Array<String>=callbackGetGlasses();
+       console.log("vasos desde Search" +glasses);
+       glasses.map(
+         (glass)=>{
+           options.push({key:glass.toLowerCase(),text:glass.toString()});
+         }
+       )
+    };
+   poblateDropdown();
      
      
     return(
--- src/components/Utils/Utils.tsx
+++ src/components/Utils/Utils.tsx
@@ -1,26 +1,30 @@
 import axios from "axios";
 
+export function callbackGetGlasses(arrayGlasses:Array<String>){
 
-export function getGlasses ():Array<String> {
+    return arrayGlasses;
+}
+
+export function getGlasses (callback:any):Array<String> {
     let outputArray:Array<String> = [];
 
 
-axios.get("https://www.thecocktaildb.com/api/json/v1/1/list.php?g=list").then(
-     
-    (response)=>{
+axios
+    .get("https://www.thecocktaildb.com/api/json/v1/1/list.php?g=list")
+    .then((response)=>{
 
         console.log(response.data.drinks);
 
         let responseDataJson=response.data.drinks;
 
-       
         for (let element in responseDataJson) {
              
             outputArray.push(responseDataJson[element].strGlass);
+
         }
-        
-       
-       
+         if(callback){
+             callback(outputArray);
+         }   
         }       
 )
 
