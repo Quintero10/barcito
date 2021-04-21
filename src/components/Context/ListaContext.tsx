c47e1da3d4cad2dc41f94c1097738dc1f0d1629d
Agustín Moragues Beckar <moraguesbeckar91@gmail.com>
2021-04-21 00:36:23 -0300

    todo

--- src/components/Context/ListaContext.tsx
+++ src/components/Context/ListaContext.tsx
@@ -3,12 +3,11 @@ import React from "react";
 import { ICategoriasContextInterface } from "./CategoriasContext";
 
 export function createCtx<A>(defaultValue: A) {
-
   type UpdateType = React.Dispatch<
     React.SetStateAction<ICategoriasContextInterface>
   >;
-  //const defaultUpdate: UpdateType = () => defaultValue;
-  const defaultUpdate:UpdateType  = getDrinkByIngredient(defaultValue);
+  const defaultUpdate: UpdateType = () => defaultValue;
+  //const defaultUpdate:UpdateType = ()=>getDrinkByIngredient(defaultValue);
   const ctx = React.createContext({
     state: defaultValue,
     update: defaultUpdate,
@@ -19,26 +18,4 @@ export function createCtx<A>(defaultValue: A) {
   }
   return [ctx, Provider] as const; // alternatively, [typeof ctx, typeof Provider]
 }
-
-const getDrinkByIngredient= (defaultValue:any) =>{
-
-  const objectICategoriasContextInterface:ICategoriasContextInterface={name:defaultValue,image:defaultValue,thumbnail:defaultValue}
-  return objectICategoriasContextInterface;
- 
-}
-
-/*
-const getDrinkByIngredient= async(defaultValue:any) =>{
-  console.log("getDrinkByIngredient from ListaContext")
-  const url = `www.thecocktaildb.com/api/json/v1/1/search.php?i=${defaultValue}`;
-
-  const categorias = await axios.get(url);
-
-  console.log(categorias);
-  let responseDataJson=categorias.data.drinks;
-  const objectICategoriasContextInterface:ICategoriasContextInterface={name:'lala',image:'sadf',thumbnail:'f'}
-  return objectICategoriasContextInterface;
- 
-}
-*/
 
